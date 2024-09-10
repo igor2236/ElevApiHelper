bc1d905178a9dc5021b26b67966cb7f72f3d9a18
igor.martins <igor.martins@PC-2-210>
2024-09-10 15:02:50 -0300

    First Commit

---
+++ ElevApiHelpper/ElevApiHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ElevApiHelper
+{
+    public class ElevApiHelper
+    {
+        public Uri _Uri;
+        public string _ApiKey;
+
+        public ElevApiHelper(Uri uri, string ApiKey)
+        {
+            _Uri = uri;
+            _ApiKey = ApiKey;
+        }
+
+        public string Discagem(int ramal, int telefone)
+        {
+            var result = DiscagemOnline.Discagem(ramal, telefone, _Uri, _ApiKey);
+            return result;
+        }
+    }
+}
