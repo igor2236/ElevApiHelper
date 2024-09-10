7b8e4feed6b3f430c9dbe1556de485288ebea941
igor.martins <igor.martins@PC-2-210>
2024-09-10 15:24:41 -0300

    Fixing parameters names: 'uri'and 'apiKey'

---
+++ ElevApiHelpper/CentroDeCusto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ElevApiHelper
+{
+    internal static class CentroDeCusto
+    { 
+
+        //GET
+​        //centro-custo​/{id}
+        //Lista o centro de custo pelo ID passado na url
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Uri"></param>
+        /// <param name="ApiKey"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object GetCentroDeCustoById(int id,Uri Uri,string ApiKey)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        internal static object PutCentroDeCusto(int id, Uri Uri, string ApiKey)
+        {
+            throw new NotImplementedException();
+        }
+        internal static object DeleteCentroDeCusto(Uri Uri, string ApiKey)
+        {
+            throw new NotImplementedException();
+        }
+        internal static object GetCentroDeCusto(Uri Uri, string ApiKey)
+        {
+            throw new NotImplementedException();
+        }
+        internal static object PostCentroDeCusto(Uri Uri, string ApiKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
