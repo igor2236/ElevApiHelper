b0f15eb551ff8f882b6d7d7ad30cd53211eca5c4
igor.martins <igor.martins@PC-2-210>
2024-09-10 15:48:15 -0300

    Adding CentroDeCusto.cs and CentroDecustoRequestBody

--- ElevApiHelpper/CentroDeCusto.cs
+++ ElevApiHelpper/CentroDeCusto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ElevApiHelper.CentroDeCustoModels;
+using System;
+using System.Collections.Concurrent;
+using System.Dynamic;
 
 namespace ElevApiHelper
 {
@@ -11,30 +14,83 @@ namespace ElevApiHelper
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="Id"></param>
-        /// <param name="Uri"></param>
-        /// <param name="ApiKey"></param>
+        /// <param name="uri"></param>
+        /// <param name="apiKey"></param>
+        /// <param name="id"></param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        internal static object GetCentroDeCustoById(int id,Uri Uri,string ApiKey)
+        internal static object GetCentroDeCustoById(Uri uri, string apiKey,int id)
         {
             throw new NotImplementedException();
         }
 
+        //PUT
+        //centro-custo/{id}
+        //Editar de centro de custo
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="apiKey"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object PutCentroDeCusto(Uri uri, string apiKey,int id)
+        {
+            throw new NotImplementedException();
+        }
 
-        internal static object PutCentroDeCusto(int id, Uri Uri, string ApiKey)
+        //DELETE
+        //centro-custo/{id}
+        //Desabilita o centro de custo do ID passado na url
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="apiKey"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object DeleteCentroDeCusto(Uri uri, string apiKey, int id)
         {
             throw new NotImplementedException();
         }
-        internal static object DeleteCentroDeCusto(Uri Uri, string ApiKey)
+
+        //GET
+        //centro-custo
+        //Pesquisa de centro de custo
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="apiKey"></param>
+        /// <param name="sort_by"></param>
+        /// <param name="order_by"></param>
+        /// <param name="nome"></param>
+        /// <param name="codigo"></param>
+        /// <param name="fk_ramal"></param>
+        /// <param name="page"></param>
+        /// <param name="rows"></param>
+        /// <param name="ativo"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object GetCentroDeCusto(Uri uri, string apiKey, string sort_by, string order_by, string nome,  int codigo, int fk_ramal, int page = 1, int rows = 5, bool ativo = true)
         {
             throw new NotImplementedException();
         }
-        internal static object GetCentroDeCusto(Uri Uri, string ApiKey)
-        {
-            throw new NotImplementedException();
-        }
-        internal static object PostCentroDeCusto(Uri Uri, string ApiKey)
+
+        //POST
+        //centro-custo
+        //Cadastro de centro de custo
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="apiKey"></param>
+        /// <param name="centroDeCustoRequestBody"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object PostCentroDeCusto(Uri uri, string apiKey,CentroDeCustoRequestBody centroDeCustoRequestBody)
         {
             throw new NotImplementedException();
         }
---
+++ ElevApiHelpper/CentroDeCustoModels/CentroDeCustoRequestBody.cs
@@ -0,0 +1,8 @@
+﻿namespace ElevApiHelper.CentroDeCustoModels
+{
+    public class CentroDeCustoRequestBody
+    {
+        public string Nome { get; set; }
+        public int Codigo { get; set; }
+    }
+}
