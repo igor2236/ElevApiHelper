15130761f61741bc33c2fe2ccf0b5061f2194ea1
igor.martins <igor.martins@PC-2-210>
2024-09-12 11:27:34 -0300

    Adding Login.cs and LoginModel.cs

---
+++ ElevApiHelper/Implementations/Login.cs
@@ -0,0 +1,29 @@
+﻿using ElevApiHelper.Models;
+using ElevApiHelper.Util;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace ElevApiHelper.Implementations
+{
+
+    internal static class Login
+    {
+
+        //POST
+        //login
+        //Verifica se retorna as informações do usúario.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="login"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object PostLogin(ElevConfig config, LoginModel login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ElevApiHelper/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElevApiHelper.Models
+{
+    internal class LoginModel
+    {
+        public string? Login { get; set; }
+        public string? Senha { get; set; }
+        public string? TipoPosicao { get; set; }
+    }
+}
