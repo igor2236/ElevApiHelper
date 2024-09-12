fd2179b8eb95c7b647e0fc605c14dde81f09d1ab
igor.martins <igor.martins@PC-2-210>
2024-09-12 11:33:17 -0300

    Adding Pausa.cs

---
+++ ElevApiHelper/Implementations/Pausa.cs
@@ -0,0 +1,40 @@
+﻿using ElevApiHelper.Util;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace ElevApiHelper.Implementations
+{
+    internal static class Pausa
+    {
+        //GET
+        //pausa/{id}
+        //Lista a pausa pelo ID passado na url
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object GetPausa(ElevConfig config, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //GET
+        //pausa
+        //Pesquisa de pausas
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static object GetPausa(ElevConfig config)
+        { 
+            throw new NotImplementedException(); 
+        }
+    }
+}
