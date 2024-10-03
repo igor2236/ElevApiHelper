eeb47aacca5e2ffd6f3b9e16f28c08abaa362eb1
igor.martins <igor.martins@PC-2-210>
2024-10-03 12:23:30 -0300

    Adding Endpoints

---
+++ ElevApiHelper/Enums/Endpoints.cs
@@ -0,0 +1,21 @@
+﻿using ElevApiHelper.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+internal static class Endpoints
+{
+    public static string CentroDeCusto = "/centro-custo";
+    public static string DiscagemOnline = "/discagem_online";
+    public static string File = "/fila";
+    public static string FilaRamal = "/fila-ramal";
+    public static string GrupoDeCaptura = "/grupo-captura";
+    public static string Grupo = "/grupo";
+    public static string Login = "/login";
+    public static string Pausa = "/pausa";
+    public static string Posicao = "/posicao";
+    public static string Ramal = "/Ramal";
+    public static string Sms = "/sms";
+    public static string DiscadorAutomatico= "/discador-automatico";
+    public static string Gravacoes= "/busca-gravacoes";
+}
